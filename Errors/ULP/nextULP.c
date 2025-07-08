d71e243c8cf9e93959a7e70f798b1c41431a2b31
Enzo Ribas <enzorochaleitedinizribas@gmail.com>
2025-07-08 10:02:55 -0300

    testes ulp

---
+++ Errors/ULP/nextULP.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    double x = pow(40,10);
+    double next = nextafter(x, INFINITY);
+
+    printf("x      = %.17g\n", x);
+    printf("nextULP = %.17g\n", next);
+    printf("ULP(x) = %.17g\n", next - x);
+
+    return 0;
+}
